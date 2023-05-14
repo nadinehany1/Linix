b930ba7e731ceb639a25332ecf6c6c50c266efd7
Nadine <109468785+nadinehany1@users.noreply.github.com>
2023-05-14 23:05:41 +0300

    Update ArrowTask.py

--- 01- Python/02- Labs/Session 2/ArrowTask.py
+++ 01- Python/02- Labs/Session 2/ArrowTask.py
@@ -1,10 +1,118 @@
-def ArrowCCW():
-    print("     *       ")
-    print("    ***      ")
-    print("   *****     ")
-    print("  *******    ")
-    print("     *       ")
-    print("     *       ")
-    print("     *       ")
+import os
+import time
+import sys
+
+def clear_console():
+    if os.name == 'nt': # for Windows
+        os.system('cls')
+
+def ArrowCCW1():
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("              *          ")
+    print("             ***         ")
+    print("            *****        ")
+    print("           *******       ")
+    print("          *********      ")
+    print("              *          ")
+    print("              *          ")
+    print("              *          ")
+    print("                         ")
+    
+def ArrowCCW2():
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("                  *      ")
+    print("                  **     ")
+    print("                  ***    ")
+    print("                  ****   ")
+    print("                  *****  ")
+    print("              ********** ")
+    print("                  *****  ")
+    print("                  ****   ")
+    print("                  ***    ")
+    print("                  **     ")
+    print("                  *      ")
+
+def ArrowCCW3():
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("              *          ")
+    print("              *          ")
+    print("              *          ")
+    print("          *********      ")
+    print("           *******       ")
+    print("            *****        ")
+    print("             ***         ")
+    print("              *          ")
+
+def ArrowCCW4():
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("                         ")
+    print("          *              ")
+    print("         **              ")
+    print("        ***              ")
+    print("       ****              ")
+    print("      *****              ")
+    print("     **********          ")
+    print("      *****              ")
+    print("       ****              ")
+    print("        ***              ")
+    print("         **              ")
+    print("          *              ")
+ 
+    # print("Enter choice 1)CW  2)CCW: ")
+
+while(1):
+    # x = input()
+    # if x=='1':
+    clear_console()
+    ArrowCCW1()
+    time.sleep(0.25) 
+    clear_console()
+    ArrowCCW2()
+    time.sleep(0.25) 
+    clear_console()
+    ArrowCCW3()
+    time.sleep(0.25) 
+    clear_console()
+    ArrowCCW4()
+    time.sleep(0.25) 
+    clear_console()          
+    # elif x=='2':
+    #         clear_console()
+    #         ArrowCCW1()
+    #         time.sleep(0.5) 
+    #         clear_console()
+    #         ArrowCCW3()
+    #         time.sleep(0.5) 
+    #         clear_console()
+    #         ArrowCCW4()
+    #         time.sleep(0.5) 
+    #         clear_console()
+    #         ArrowCCW2()
+    #         time.sleep(0.5)
+    #         clear_console()
+    # else:
+    #     print("Invalide Input ")
+    # sys.exit(0)
+            
+        
+
 
 
